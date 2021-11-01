023155e29112ab30348b4a2d7427706011c1b239
Paul Schifferer <paul@schifferers.net>
2021-10-31 21:35:04 -0700

    Test contexts

--- tests/test_context_helper.py
+++ tests/test_context_helper.py
@@ -4,8 +4,13 @@ __author__ = "Paul Schifferer <dm@sweetrpg.com>"
 """
 
 from sweetrpg_web_core.helpers.context import get_context
+from flask import Flask, current_app
+
+
+app = Flask(__name__)
 
 
 def test_get_context():
-    context = get_context()
-    assert isinstance(context, dict)
+    with app.test_request_context():
+        context = get_context()
+        assert isinstance(context, dict)
