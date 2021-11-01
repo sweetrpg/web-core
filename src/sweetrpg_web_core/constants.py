88ae0506292fe70e153d4b570ca7f6c65799b48c
Paul Schifferer <paul@schifferers.net>
2021-10-31 21:27:36 -0700

    User info

---
+++ src/sweetrpg_web_core/constants.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__author__ = "Paul Schifferer <dm@sweetrpg.com>"
+"""Web exceptions
+"""
+
+
+SESSION_ACCESS_TOKEN = "access_token"
+SESSION_EMAIL = "email"
+SESSION_USER_ID = "user_id"
--- src/sweetrpg_web_core/helpers/context.py
+++ src/sweetrpg_web_core/helpers/context.py
@@ -4,12 +4,16 @@ __author__ = "Paul Schifferer <dm@sweetrpg.com>"
 Context helper functions.
 """
 
-from flask import current_app
+from flask import current_app, session
 import os
+from sweetrpg_web_core import constants
 
 
 def get_context() -> dict:
     return {
-        'user': {}, # TODO current_app.session['user']???
+        'user': {
+            'id': session.get(constants.SESSION_EMAIL),
+            'email': session.get(constants.SESSION_USER_ID),
+        },
         'base_path': os.environ.get('APPLICATION_BASE_PATH') or ""
     }
