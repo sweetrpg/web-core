c077133186e1695195b93d578e39a2d0b48b6cc4
Paul Schifferer <paul@schifferers.net>
2021-11-06 16:02:40 -0700

    Add hook registration function.

--- src/sweetrpg_web_core/blueprints/health/__init__.py
+++ src/sweetrpg_web_core/blueprints/health/__init__.py
@@ -13,15 +13,37 @@ from sweetrpg_web_core import constants
 
 blueprint = Blueprint("health", __name__, url_prefix="/health")
 
+_health_check_service_hooks = {}
+
+
+def register_health_check_service_hook(name: str, callable) -> None:
+    """Add a callable service hook for the health check endpoint.
+
+    :param name: The name of the hook.
+    :param callable: A callable that will return a JSON-encodable result. If this is `None`,
+        the hook will be deregistered.
+    """
+    if callable is None:
+        del _health_check_service_hooks[name]
+    else:
+        _health_check_service_hooks[name] = callable
+
 
 @blueprint.route("/status")
 def health_check():
-    r = {'services':{}}
+    r = {'services': {}}
     build_info_path = os.environ.get(constants.BUILD_INFO_PATH)
     if build_info_path:
         with open(build_info_path, "r") as bi:
             build_info = json.load(bi)
             r["build"] = build_info
+
+    for k,v in _health_check_service_hooks.items():
+        try:
+            result = v()
+            r['services'][k] = result
+        except:
+            r['services'][k] = 'Unknown'
 
     return r
 
--- src/sweetrpg_web_core/constants.py
+++ src/sweetrpg_web_core/constants.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __author__ = "Paul Schifferer <dm@sweetrpg.com>"
-"""Web exceptions
+"""Constants.
 """
 
 
