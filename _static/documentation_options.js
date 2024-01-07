85e04f73d055725c9fa57ed8eea26cc640ec8375
paulyhedral <paulyhedral@users.noreply.github.com>
2024-01-07 00:28:21 +0000

    deploy: 1af4c322a94c7c535aac2de0dfdc6a58bb2c3fca

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.493',
+    VERSION: '0.0.494',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
